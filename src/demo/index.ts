4af080de81f0c2f5d50b957238159a7a092410fe
Craig Miller <craigmiller160@gmail.com>
2022-02-12 14:27:48 -0500

    Refactored request type

--- src/demo/index.ts
+++ src/demo/index.ts
@@ -1,16 +1,16 @@
 /* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars */
-import { FnRequest } from '../routing/components/FnRequest';
+import { SimpleRequest } from '../routing/components/FnRequest';
 import { FnResponse } from '../routing/components/FnResponse';
 import { Routes } from '../routing/Routes';
 
-const basic = (req: FnRequest<any>): FnResponse<any> => {
+const basic = (req: SimpleRequest): FnResponse<any> => {
 	return {
 		status: 200,
 		body: 'Hello World'
 	};
 };
 
-const promise = (req: FnRequest<any>): Promise<FnResponse<any>> =>
+const promise = (req: SimpleRequest): Promise<FnResponse<any>> =>
 	new Promise((resolve) => {
 		setTimeout(() => {
 			resolve({
@@ -20,18 +20,18 @@ const promise = (req: FnRequest<any>): Promise<FnResponse<any>> =>
 		}, 2000);
 	});
 
-const error = (req: FnRequest<any>): FnResponse<any> => {
+const error = (req: SimpleRequest): FnResponse<any> => {
 	throw new Error('Dying');
 };
 
-const errorPromise = (req: FnRequest<any>): Promise<FnResponse<any>> =>
+const errorPromise = (req: SimpleRequest): Promise<FnResponse<any>> =>
 	new Promise((resolve, reject) => {
 		setTimeout(() => {
 			reject(new Error('Dying Async'));
 		}, 2000);
 	});
 
-const errorReturned = (req: FnRequest<any>): FnResponse<any> => {
+const errorReturned = (req: SimpleRequest): FnResponse<any> => {
 	return {
 		error: new Error('Returned Dying')
 	};
--- src/routing/components/FnRequest.ts
+++ src/routing/components/FnRequest.ts
@@ -1,10 +1,12 @@
 import { UriInfo } from './UriInfo';
 import { Session } from './Session';
 
-export interface FnRequest<ReqB> {
+interface FnRequest<ReqB> {
 	readonly uriInfo: UriInfo;
 	readonly body: ReqB;
 	readonly headers: object; // TODO type this
 	readonly cookies: object; // TODO type this
 	readonly session?: Session;
 }
+
+export type SimpleRequest = FnRequest<never>;
