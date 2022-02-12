86380983937a9dbabcdbd820a095ad6b772dc0b5
Craig Miller <craigmiller160@gmail.com>
2022-02-12 10:09:56 -0500

    Refactored how the server is created

--- src/server/ServerCreator.ts
+++ src/server/ServerCreator.ts
@@ -3,15 +3,21 @@ import express, { Express } from 'express';
 import { RouteHandler } from '../routing/RouteHandler';
 import { translate } from '../routing/RouteTranslator';
 
+export type Route<Return> = (uri: string, routeHandler: RouteHandler) => Return;
+
 // TODO add more HTTP methods
 export interface FnRouter {
-	readonly get: (uri: string, routeHandler: RouteHandler) => void;
-	readonly post: (uri: string, routeHandler: RouteHandler) => void;
-	readonly put: (uri: string, routeHandler: RouteHandler) => void;
-	readonly delete: (uri: string, routeHandler: RouteHandler) => void;
+	readonly get: Route<FnRouter>;
+	readonly post: Route<FnRouter>;
+	readonly put: Route<FnRouter>;
+	readonly delete: Route<FnRouter>;
 }
 
-export interface FnServer extends FnRouter {
+export interface FnServer {
+	readonly get: Route<FnServer>;
+	readonly post: Route<FnServer>;
+	readonly put: Route<FnServer>;
+	readonly delete: Route<FnServer>;
 	readonly route: (baseUri: string) => FnRouter;
 	readonly listen: (port: number) => Promise<void>;
 }
@@ -22,19 +28,39 @@ export const createServer = (
 	const app = express();
 	configureExpress?.(app);
 	return {
-		get: (uri, routeHandler) => app.get(uri, translate(routeHandler)),
-		post: (uri, routeHandler) => app.post(uri, translate(routeHandler)),
-		put: (uri, routeHandler) => app.put(uri, translate(routeHandler)),
-		delete: (uri, routeHandler) => app.delete(uri, translate(routeHandler)),
+		get(uri, routeHandler) {
+			app.get(uri, translate(routeHandler));
+			return this;
+		},
+		post(uri, routeHandler) {
+			app.post(uri, translate(routeHandler));
+			return this;
+		},
+		put(uri, routeHandler) {
+			app.put(uri, translate(routeHandler));
+			return this;
+		},
+		delete(uri, routeHandler) {
+			app.delete(uri, translate(routeHandler));
+			return this;
+		},
 		route: (baseUri) => ({
-			get: (uri, routeHandler) =>
-				app.get(`${baseUri}${uri}`, translate(routeHandler)),
-			post: (uri, routeHandler) =>
-				app.post(`${baseUri}${uri}`, translate(routeHandler)),
-			put: (uri, routeHandler) =>
-				app.put(`${baseUri}${uri}`, translate(routeHandler)),
-			delete: (uri, routeHandler) =>
-				app.delete(`${baseUri}${uri}`, translate(routeHandler))
+			get(uri, routeHandler) {
+				app.get(`${baseUri}${uri}`, translate(routeHandler));
+				return this;
+			},
+			post(uri, routeHandler) {
+				app.post(`${baseUri}${uri}`, translate(routeHandler));
+				return this;
+			},
+			put(uri, routeHandler) {
+				app.put(`${baseUri}${uri}`, translate(routeHandler));
+				return this;
+			},
+			delete(uri, routeHandler) {
+				app.delete(`${baseUri}${uri}`, translate(routeHandler));
+				return this;
+			}
 		}),
 		listen: (port) => new Promise((resolve) => app.listen(port, resolve))
 	};
