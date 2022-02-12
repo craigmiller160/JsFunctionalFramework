6b37c918bc73945cec2325470409699b81055a6c
Craig Miller <craigmiller160@gmail.com>
2022-02-12 08:40:34 -0500

    Code cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 import express from 'express';
-import { createConcept1 } from './concept1';
 
 const app = express();
 app.get('/hello', (req, res) => {
 	res.send('Hello World');
 });
-createConcept1(app);
 
 app.listen(8080, () => {
 	console.error('Listening on port 8080');
