6ccbbae5413c63188bdfd083b2d8166b3a6fab09
Craig Miller <craigmiller160@gmail.com>
2022-02-12 09:57:31 -0500

    Added nested routes

--- src/server/ServerCreator.ts
+++ src/server/ServerCreator.ts
@@ -4,12 +4,15 @@ import { RouteHandler } from '../routing/RouteHandler';
 import { translate } from '../routing/RouteTranslator';
 
 // TODO add more HTTP methods
-// TODO add nested routers
-export interface FnServer {
+export interface FnRouter {
 	readonly get: (uri: string, routeHandler: RouteHandler) => void;
 	readonly post: (uri: string, routeHandler: RouteHandler) => void;
 	readonly put: (uri: string, routeHandler: RouteHandler) => void;
 	readonly delete: (uri: string, routeHandler: RouteHandler) => void;
+}
+
+export interface FnServer extends FnRouter {
+	readonly route: (baseUri: string) => FnRouter;
 }
 
 export const createServer = (
@@ -21,6 +24,16 @@ export const createServer = (
 		get: (uri, routeHandler) => app.get(uri, translate(routeHandler)),
 		post: (uri, routeHandler) => app.post(uri, translate(routeHandler)),
 		put: (uri, routeHandler) => app.put(uri, translate(routeHandler)),
-		delete: (uri, routeHandler) => app.delete(uri, translate(routeHandler))
+		delete: (uri, routeHandler) => app.delete(uri, translate(routeHandler)),
+		route: (baseUri) => ({
+			get: (uri, routeHandler) =>
+				app.get(`${baseUri}${uri}`, translate(routeHandler)),
+			post: (uri, routeHandler) =>
+				app.post(`${baseUri}${uri}`, translate(routeHandler)),
+			put: (uri, routeHandler) =>
+				app.put(`${baseUri}${uri}`, translate(routeHandler)),
+			delete: (uri, routeHandler) =>
+				app.delete(`${baseUri}${uri}`, translate(routeHandler))
+		})
 	};
 };
